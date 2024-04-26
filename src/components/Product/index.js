5616b62287ff39f0bf7c91d1c49cbec45a181227
Trevor Bisson <tbisson@corebridge.net>
2024-04-26 16:26:00 -0500

    added faux data to product cards

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,11 +1,54 @@
 import "./Product.scss";
 
 const Product = ()=>{
+    const productData = [
+
+        {
+            pName:"Jacket",
+            pPrice:65,
+            pImg: "shop-1.jpg"
+        },
+        {
+            pName:"Purse",
+            pPrice:205,
+            pImg: "shop-2.jpg"
+        },
+        {
+            pName:"Dress",
+            pPrice:85,
+            pImg: "shop-3.jpg"
+        },
+        {
+            pName:"Distressed Jeans",
+            pPrice:43,
+            pImg: "shop-4.jpg"
+        },
+        {
+            pName:"Boots",
+            pPrice:315,
+            pImg: "shop-5.jpg"
+        },
+        {
+            pName:"Ugly Backpack",
+            pPrice:117,
+            pImg: "shop-6.jpg"
+        },
+    ]
+
     return (
-        <div className="Product">
-            <img src={require("../../assests/images/shop/shop-1.jpg")}></img>
-            <a href="#">Jacket</a>
-            <p>$45</p>
+        <div className="AllProductsWrapper">
+        {
+            productData.map((product,key)=>{
+                return(
+                    <div className="Product">
+                        <img src={require("../../assests/images/shop/"+product.pImg)}></img>
+                        <a href="#">{product.pName}</a>
+                        <p>${product.pPrice}</p>
+                    </div>
+
+                )
+            })
+        }
         </div>
     )
 }
