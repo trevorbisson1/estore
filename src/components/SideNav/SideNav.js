1ff4f2a48b048fd9acade2c714f56572215aed61
Trevor Bisson <4trevorbisson@gmail.com>
2024-04-29 14:29:59 -0500

    finished accordian sidenav update

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -10,25 +10,34 @@ const SideNav = () => {
             <h3 className="SectionTitle">Categories</h3>
 
             <div className='accordion'>
-                <div className='accordion-item individual-category' >
-                    <div className='accordion-header'>
-                        <button className='accordion-button' data-bs-target="#accordion-heading-one" data-bs-toggle="collapse">
-                            <div className='category-title'>
-                                <a href='#'>Men</a>
+                {
+                    accordianData.map((accordianCategory,key)=>{
+                        return (
+                            <div className='accordion-item individual-category' >
+                                <div className='accordion-header'>
+                                    <button className='accordion-button' data-bs-target={"#collapse"+key} data-bs-toggle="collapse">
+                                        <div className='category-title'>
+                                            <a href='#'>{accordianCategory.category}</a>
+                                        </div>
+                                    </button>
+                                </div>
+                                <div className='accordion-collapse collapse show' id={"collapse"+key}>
+                                    <div className='accordion-body'>
+                                        <ul>
+                                            {
+                                                accordianCategory.items.map((item)=>{
+                                                    return <li className='sub-items'> <a href='#'>{item}</a> </li>
+                                                })
+                                            }
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
-                        </button>
-                    </div>
-                    <div className='accordion-collapse collapse show' id="accordion-heading-one">
-                        <div className='accordion-body'>
-                            <ul>
-                                <li className='sub-items'> <a href='#'>Coats</a> </li>
-                                <li className='sub-items'> <a href='#'>Boots</a> </li>
-                                <li className='sub-items'> <a href='#'>Party Wear</a> </li>
-                                <li className='sub-items'> <a href='#'>Shirts</a> </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+
+                        )
+
+                    })
+                }
             </div>
         
         </div>
