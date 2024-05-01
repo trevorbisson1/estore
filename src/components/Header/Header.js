3f31bc206f8feef2951f1e1f17585a0ffde66deb
Trevor Bisson <4trevorbisson@gmail.com>
2024-05-01 16:42:50 -0500

    added cart badge for total item counts

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
 import SearchBar from "./Search";
 import "./Header.css"
+import { useSelector } from "react-redux";
 
 
 function Header(){
+
+    const cartItemCount = useSelector(state=>state.cr.totalQuantity);
+
     return (
         <div className="Header">
             <div className="Header-Name">
@@ -14,7 +18,19 @@ function Header(){
                     <h5><a href="#">Login</a></h5>
                     <h5><a href="#">Register</a></h5> 
                     <i className="fa fa-heart"/>
-                    <i className="fa fa-shopping-cart"/>
+                    <div className="cart-n-counter">
+                        <i className="fa fa-shopping-cart"/>
+                        {
+                            
+                            cartItemCount !==0 ? 
+                        <div id="cart-item-counter">
+                        <p>{cartItemCount}</p> 
+                        </div>
+                        
+                        : <></>
+                        
+                        }
+                    </div>
                 </div>
             </div>
         </div>
